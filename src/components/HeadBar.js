e64f22b35b0a21e7f459f25a18b4488fce3535b6
Bhargav Guggilapu <bhargavguggilapu@gmail.com>
2024-09-29 18:41:55 -0500

    initial login

--- src/components/HeadBar.js
+++ src/components/HeadBar.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "./Helpers/Button";
 import { COLORS } from "./Constants/Constants";
+import logo from "../images/logo.jpg";
 
 export default function HeadBar() {
   const [greeting, setGreeting] = useState("");
@@ -18,12 +19,18 @@ export default function HeadBar() {
   return (
     <div className="flex bg-gradient-to-r from-orange-500 to-green-500 text-white shadow-md">
       <div
-        className="w-64 p-4 text-2xl font-bold border-r border-white/20"
+        className="w-64 p-4 border-r border-white/20 flex items-center"
         style={{ fontFamily: "Rajdhani, sans-serif" }}
       >
-        CrossConnect
+        <Avatar alt="logo" src={logo} />
+        <h1
+          className="text-2xl text-white font-bold ml-3"
+          style={{ fontFamily: "Rajdhani, sans-serif" }}
+        >
+          CrossConnect
+        </h1>
       </div>
-      <header className="flex-1 flex justify-between items-center px-4">
+      <header className="flex-1 flex justify-between items-center px-4 ml-2">
         <h1
           className="text-2xl font-bold"
           style={{ fontFamily: "Rajdhani, sans-serif" }}
