ffb79dda45dde914cca390bea09c2da55b19de22
Bhargav Guggilapu <93968760+bhargav-guggilapu@users.noreply.github.com>
2024-10-15 18:13:14 -0500

    Update EndPoints.js

--- src/services/EndPoints.js
+++ src/services/EndPoints.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://cross-connect.onrender.com";
+export const BACKEND_URL = "https://crossconnect-rur7ixi6.b4a.run";
 export const BACKEND_URL_LOCAL = "http://localhost:3001";
 
 export const API_ENDPOINTS = {
