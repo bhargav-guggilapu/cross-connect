9cc82aef4419255521fa30360a082019abe06e73
Bhargav Guggilapu <bhargavguggilapu@gmail.com>
2024-11-07 20:14:16 -0600

    build commit

--- src/services/EndPoints.js
+++ src/services/EndPoints.js
@@ -1,6 +1,3 @@
-export const BACKEND_URL = "https://crossconnect-rur7ixi6.b4a.run";
-export const BACKEND_URL_LOCAL = "http://localhost:3001";
-
 export const API_ENDPOINTS = {
   USERS: "/users",
   PROHIBITED_ITEMS: "/prohibitedItems",
